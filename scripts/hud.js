c2934b47e2f11301293eb991a17c7ec17446675c
Eva <EvanesceExotica@gmail.com>
2021-09-27 22:18:21 -0400

    got the hud working generally

--- scripts/hud.js
+++ scripts/hud.js
@@ -2,51 +2,64 @@
 
 let hud;
 let controlled = false;
-Hooks.on("hoverToken", (token, mouseIn) => {
+// Hooks.on("hoverToken", (token, mouseIn) => {
+// 	let ourToken = token;
+// 	// getAttacks(ourToken);
+// 	if (mouseIn && !controlled) {
+// 		//if we're hovering, render the hud
+// 		hud = new Hud(ourToken).render(true);
+// 	} else {
+// 		if (!controlled) {
+// 			//if we're not controlling it, and we're no longer hovering
+// 			//close the hud
+// 			hud.close();
+// 		}
+// 	}
+// });
+Hooks.on("controlToken", (token, isControlled)=> {
 	let ourToken = token;
-	// getAttacks(ourToken);
-	if (mouseIn) {
-		//if we're hovering, render the hud
+	if(isControlled){
 		hud = new Hud(ourToken).render(true);
-	} else {
-		if (!controlled) {
-			//if we're not controlling it, and we're no longer hovering
-			//close the hud
+	}
+	else{
+		if(hud){
 			hud.close();
 		}
 	}
-});
-Hooks.on("controlToken", (token, isControlled) => {
-	let ourToken = token;
-	if (ourToken == hud.ourToken) {
-		//if we're now controlling 
-		// the same token we're currently hovering,
-		//set controlled to true
-		if (isControlled) {
-			controlled = true;
-		}
-		else{
-			//if we're not controlling it any longer, close the hud
-			controlled = false;
-			hud.close();
-		}
-	}
+})
+// Hooks.on("controlToken", (token, isControlled) => {
+// 	let ourToken = token;
+// 	if (ourToken == hud.ourToken) {
+// 		//if we're now controlling 
+// 		// the same token we're currently hovering,
+// 		//set controlled to true
+// 		if (isControlled) {
+// 			controlled = true;
+// 		}
+// 		else{
+// 			//if we're not controlling it any longer, close the hud
+// 			controlled = false;
+// 			hud.close();
+// 		}
+// 	}
+// 	else{
+// 		if (isControlled) {
+// 			hud = new Hud(ourToken).render(true);
+// 			controlled = true;
+// 		}
+// 		else {
+// 			controlled = false;
+// 			hud.close();
+// 		}
+// 	}
 
-});
+// });
 
-function getAttacks(ourToken) {
 
-	if (ourToken.data.actorLink) {
-		let actor = game.actors.get(ourToken.data.actorId);
-		// console.log(actor.data.items);
-		let attacks = actor.data.items.contents.filter((item) => {
-			return item.data.type === "attack";
-		});
-	}
-}
 
 export class Hud extends Application {
 	constructor(object) {
+		// super(object);
 		super();
 		this.ourToken = object;
 		this.attacks = this.getAttacks(this.ourToken);
@@ -55,23 +68,22 @@ export class Hud extends Application {
 	/** @override */
 	static get defaultOptions() {
 		return mergeObject(super.defaultOptions, {
-			template: "/modules/hud-and-trackers/templates/hud.html",
-			id: "hud",
-			classes: [],
-			width: 200,
-			height: 20,
-			left: 900,
-			top: 2000,
-			scale: 1,
-			// background: "none",
-			popOut: false,
+			// classes: ['form'],
+			popOut: true,
+			submitOnChange: false,
+			closeOnSubmit: false,
 			minimizable: false,
-			resizable: false,
-			title: "hud",
-			dragDrop: [],
-			tabs: [],
-			scrollY: [],
+			resizable: true,
+			background: "none",
+			template: 'modules/hud-and-trackers/templates/hud.html',
+			id: 'tokenHud',
+			title: 'tokenHud',
+			onSubmit: (e) => e.preventDefault(),
 		});
+	}
+	async _updateObject(event, formData) {
+
+		this.render();
 	}
 
 	getData() {
@@ -81,10 +93,19 @@ export class Hud extends Application {
 	}
 
 	activateListeners(html) {
+		super.activateListeners(html);
 		let hudItems = html.find(".hud-item");
-		// for(let hudItem in hudItems){
-		// 	hudItems.addEventListener()
-		// }
+		for(let hudItem of hudItems){
+			hudItem.addEventListener("mouseover", (event)=> {
+				console.log("mouse in!!!!");
+			})
+		}
+	let hudBtns = html.find(".hud-item__button");
+		for(let btn of hudBtns){
+			btn.addEventListener("click", (event)=> {
+				console.log("CLICK");
+			})
+		}
 
 	}
 	getAttacks(ourToken) {
@@ -99,3 +120,4 @@ export class Hud extends Application {
 		}
 	}
 }
+window.hud = hud;
