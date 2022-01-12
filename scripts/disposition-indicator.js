70fa3cfc7c5a6e4892d95bc766ef7a0b11913506
Eva <EvanesceExotica@gmail.com>
2022-01-12 18:36:17 -0500

    added disposition indicators that update when disposition changed

--- scripts/disposition-indicator.js
+++ scripts/disposition-indicator.js
@@ -1,6 +1,14 @@
 Hooks.on("canvasReady", () => {
-    let tokenDocs = game.scenes.viewed.getEmbeddedCollection("Token").contents;
-    tokenDocs.forEach((tokenDoc) => createDispositionMarker(tokenDoc));
+    if (game.user.isGM) {
+        let tokenDocs = game.scenes.viewed.getEmbeddedCollection("Token").contents;
+        tokenDocs.forEach((tokenDoc) => createDispositionMarker(tokenDoc));
+    }
+});
+Hooks.on("updateToken", (doc, change, options) => {
+    console.log("Disposition is ", change.disposition);
+    if (change.disposition && game.user.isGM) {
+        createDispositionMarker(doc);
+    }
 });
 
 function createDispositionMarker(tokenDoc) {
@@ -9,10 +17,26 @@ function createDispositionMarker(tokenDoc) {
     }
     let token = tokenDoc.object;
     let disposition = tokenDoc.data.disposition;
-    console.log(token, disposition);
 
     let texturePath;
-    texturePath = "modules/hud-and-trackers/images/convergence-target.png";
+    let color;
+    switch (disposition) {
+        case 1:
+            //friendly
+            color = "0x99FFCC";
+            texturePath = "modules/hud-and-trackers/images/hearts.png";
+            break;
+        case 0:
+            //neutral
+            color = "0x9999FF";
+            texturePath = "/modules/hud-and-trackers/images/person.png";
+            break;
+        case -1:
+            //hostile
+            texturePath = "/modules/hud-and-trackers/images/hades-symbol.png";
+            color = "0xCC0033";
+    }
+    // texturePath = "modules/hud-and-trackers/images/convergence-target.png";
     if (token.marker) {
         //destroy the marker PIXI Container stored on the token
         token.marker.destroy();
@@ -21,31 +45,17 @@ function createDispositionMarker(tokenDoc) {
         delete token.marker;
     }
 
-    token.marker = token.addChildAt(
-        new PIXI.Container(),
-        token.getChildIndex(token.icon)
-    );
+    token.marker = token.addChild(new PIXI.Container());
+    // token.marker = token.addChildAt(new PIXI.Container(), 0);
     const sprite = PIXI.Sprite.from(texturePath);
     token.marker.addChild(sprite);
     sprite.zIndex = 2000;
-    sprite.width = 200;
-    sprite.height = 200;
+    sprite.width = 25;
+    sprite.height = 25;
+    // sprite.anchor.set(-0.25, -0.25);
+    sprite.position.set(0, 0);
     sprite.anchor.set(0.5, 0.5);
-    sprite.position.set(token.w / 2, token.h / 2);
+    // sprite.position.set(token.w / 4, token.h / 4);
 
-    let color;
-    switch (disposition) {
-        case 1:
-            //friendly
-            color = "0x99FFCC";
-            break;
-        case 0:
-            //neutral
-            color = "0x9999FF";
-            break;
-        case -1:
-            //hostile
-            color = "0xCC0033";
-    }
     token.marker.children.forEach((child) => (child.tint = color));
 }
