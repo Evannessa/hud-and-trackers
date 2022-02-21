7e4d674bdbe43e85d1b144159f74f4c290cdad71
Eva <EvanesceExotica@gmail.com>
2022-02-21 14:44:33 -0500

    added cleaner syntax using html.on

--- scripts/journal-tooltips.js
+++ scripts/journal-tooltips.js
@@ -1,19 +1,20 @@
-let journalName = "Alresch";
+let keywords;
+fetch("/modules/hud-and-trackers/data/icon-glossery-data.json")
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => (keywords = data));
+// .then((data) => console.log(data));
+// const data = require("/modules/hud-and-trackers/data/icon-glossery-data.json");
+// const keywords = data;
+let journalName = "ICON TEST";
 Hooks.on("canvasReady", () => {
     game.journal.getName(journalName).sheet.render(true);
 });
-const keywords = [
-    {
-        Alresch: "Hail my name is Alresch, King of the Deep",
-    },
-    {
-        priest: "There's stuff about priests here",
-    },
-    {
-        Damage: `Damage in ICON depends on your job. Every class has a damage die (d6, d8, etc) which is often written as [D], and a fray damage value (a fixed amount).`,
-    },
-];
+//maybe add tooltip entity link if there is one
 Hooks.on("renderJournalSheet", (ourObject, html, data) => {
+    //go through all of our keywords, and highlight them, and give
+    //event listeners to show tooltips on hover
     for (let keyword of keywords) {
         highlightKeywords(html, keyword);
     }
@@ -21,8 +22,9 @@ Hooks.on("renderJournalSheet", (ourObject, html, data) => {
 
 async function highlightKeywords(html, keyword) {
     //TODO: Find keywords, highlight them
-    let searchTerm = Object.keys(keyword)[0];
-    console.log("Current search Term", searchTerm, "Keyword", keyword);
+    let searchTerm = keyword.Term.trim();
+    let definition = keyword.Definition.trim();
+    // console.log("Current search Term", searchTerm, "Definition", definition);
     let pattern = `(${searchTerm})`;
     let regexTerm = new RegExp(pattern);
 
@@ -32,33 +34,51 @@ async function highlightKeywords(html, keyword) {
     let paragraphs = [...html[0].querySelectorAll("p")].filter((p) =>
         p.textContent.includes(searchTerm)
     );
+
+    let className = searchTerm.replaceAll(/[^\w\s]/gi, "").replace(/ /g, "");
     paragraphs.forEach((p) =>
         $(p).html((i, html) => {
             return html.replace(
                 regexTerm,
-                `<a href="#" class="showTooltip ${searchTerm}">${searchTerm}</a>`
+                `<a href="#" class="showTooltip ${className}">${searchTerm}</a>`
             );
         })
     );
     paragraphs.forEach((p) => $(p).addClass("journalTooltipParagraph"));
     //select them all
-    html[0].querySelectorAll(`.showTooltip.${searchTerm}`).forEach((element) => {
-        return $(element)
-            .mouseover((event) => {
-                renderTooltip(
-                    searchTerm,
-                    keyword[searchTerm],
-                    // event.currentTarget
-                    $(event.currentTarget).closest(".window-app")
-                );
-            })
-            .mouseleave((event) => {
-                hideTooltip(
-                    // event.currentTarget);
-                    $(event.currentTarget).closest(".window-app")
-                );
-            });
+    html[0].querySelectorAll(`.showTooltip.${className}`).forEach((element) => {
+        console.log("This element will render a tooltip", element);
     });
+    $(html[0]).on("mouseover", `.showTooltip.${className}`, (event) => {
+        renderTooltip(
+            searchTerm,
+            definition,
+            $(event.currentTarget).closest(".window-app")
+        );
+    });
+
+    $(html[0]).on("mouseleave", `.showTooltip.${className}`, (event) => {
+        hideTooltip($(event.currentTarget).closest(".window-app"));
+    });
+    // setTimeout(() => {
+    //     html[0].querySelectorAll(`.showTooltip.${className}`).forEach((element) => {
+    //         return $(element)
+    //             .mouseover((event) => {
+    //                 renderTooltip(
+    //                     searchTerm,
+    //                     definition,
+    //                     // event.currentTarget
+    //                     $(event.currentTarget).closest(".window-app")
+    //                 );
+    //             })
+    //             .mouseleave((event) => {
+    //                 hideTooltip(
+    //                     // event.currentTarget);
+    //                     $(event.currentTarget).closest(".window-app")
+    //                 );
+    //             });
+    //     });
+    // }, 1000);
 }
 
 async function hideTooltip(keywordElement) {
@@ -70,6 +90,7 @@ async function renderTooltip(title, description, keywordElement) {
         title: title,
         description: description,
     };
+    console.log("Rendering", data);
     const tooltipHtml = await renderTemplate(template, { data: data });
     $(keywordElement).append(tooltipHtml);
 }
