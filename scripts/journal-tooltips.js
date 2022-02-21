4b3d3a70588d910f8a91bf161a61be6e9cd44079
Eva <EvanesceExotica@gmail.com>
2022-02-20 19:49:01 -0500

    added functionality for multiple keywords

--- scripts/journal-tooltips.js
+++ scripts/journal-tooltips.js
@@ -7,41 +7,60 @@ const keywords = [
         Alresch: "Hail my name is Alresch, King of the Deep",
     },
     {
+        priest: "There's stuff about priests here",
+    },
+    {
         Damage: `Damage in ICON depends on your job. Every class has a damage die (d6, d8, etc) which is often written as [D], and a fray damage value (a fixed amount).`,
     },
 ];
 Hooks.on("renderJournalSheet", (ourObject, html, data) => {
+    for (let keyword of keywords) {
+        highlightKeywords(html, keyword);
+    }
+});
+
+async function highlightKeywords(html, keyword) {
     //TODO: Find keywords, highlight them
-    let searchTerm = Object.keys(keywords[0]);
+    let searchTerm = Object.keys(keyword)[0];
+    console.log("Current search Term", searchTerm, "Keyword", keyword);
     let pattern = `(${searchTerm})`;
     let regexTerm = new RegExp(pattern);
-    let array = [...html];
-    [...html[0].querySelectorAll("p")]
-        .filter((p) => p.textContent.includes(Object.keys(keywords[0])))
-        .forEach((p) =>
-            $(p).html((i, html) => {
-                return html.replace(
-                    regexTerm,
-                    `<a href="#" class="showTooltip">${searchTerm}</a>`
-                );
-            })
-        );
-    html[0].querySelectorAll(".showTooltip").forEach((element) => {
+
+    //find all the paragraphs in the children, filter them for ones whose text content
+    //includes the keyword, then use html.replace() with regex to turn them into anchor tags
+    //w/ show-tooltip class
+    let paragraphs = [...html[0].querySelectorAll("p")].filter((p) =>
+        p.textContent.includes(searchTerm)
+    );
+    paragraphs.forEach((p) =>
+        $(p).html((i, html) => {
+            return html.replace(
+                regexTerm,
+                `<a href="#" class="showTooltip ${searchTerm}">${searchTerm}</a>`
+            );
+        })
+    );
+    paragraphs.forEach((p) => $(p).addClass("journalTooltipParagraph"));
+    //select them all
+    html[0].querySelectorAll(`.showTooltip.${searchTerm}`).forEach((element) => {
         return $(element)
             .mouseover((event) => {
                 renderTooltip(
                     searchTerm,
-                    keywords[0][searchTerm],
+                    keyword[searchTerm],
+                    // event.currentTarget
                     $(event.currentTarget).closest(".window-app")
                 );
             })
             .mouseleave((event) => {
-                hideTooltip($(event.currentTarget).closest(".window-app"));
+                hideTooltip(
+                    // event.currentTarget);
+                    $(event.currentTarget).closest(".window-app")
+                );
             });
     });
+}
 
-    const title = keywords[0];
-});
 async function hideTooltip(keywordElement) {
     $(keywordElement).children(".journal-tooltip").remove();
 }
@@ -52,7 +71,6 @@ async function renderTooltip(title, description, keywordElement) {
         description: description,
     };
     const tooltipHtml = await renderTemplate(template, { data: data });
-    // console.log($(keywordElement).closest(".journal-sheet"));
     $(keywordElement).append(tooltipHtml);
 }
 
